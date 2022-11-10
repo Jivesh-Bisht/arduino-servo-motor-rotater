4b80f0ea4aa4df2a2e97a9814ffab2cdb1cec047
Jivesh-Bisht <110481740+Jivesh-Bisht@users.noreply.github.com>
2022-11-10 16:08:54 +0530

    main.ino

---
+++ main.ino
@@ -0,0 +1,25 @@
+// Add th control pin at 3 or else change the code according the control pin is in the board number
+
+
+// Include the Servo library 
+#include <Servo.h> 
+// Change the Servo pin if needed
+int servoPin = 3; 
+
+Servo Servo1; 
+void setup() { 
+   Servo1.attach(servoPin); 
+}
+void loop(){ 
+//this code will trun the servo motor 0 degress and wait 1 seconds or 1000ms
+   Servo1.write(0); 
+   delay(1000); 
+   
+//this code will trun the servo motor 90 degress and wait 1 seconds or 1000ms
+   Servo1.write(90); 
+   delay(1000); 
+   
+//this code will trun the servo motor 180 degress and wait 1 seconds or 1000ms
+   Servo1.write(180); 
+   delay(1000); 
+}
