db59a2f72a41511ac2e722855d3aaf1425109373
Jivesh-Bisht <110481740+Jivesh-Bisht@users.noreply.github.com>
2022-11-14 21:25:52 +0530

    Update main.ino
    did it the last night

--- main.ino
+++ main.ino
@@ -1,24 +1,66 @@
-// Add th control pin at 3 or else change the code according the control pin is in the board number
+// Add the control pin at 9 or else change the code according the control pin is in the board number
 
 
 // Include the Servo library 
 #include <Servo.h>
 
-Servo myservo;  // create servo object to control a servo
-
+Servo servo1, servo2;  // create servo object to control a servo
+int ButtonPin = 2;
+int counter = 0;
+bool pressed = false;
 int pos = 0;    // variable to store the servo position
 
 void setup() {
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  Serial.begin(115200);
+  pinMode(ButtonPin, INPUT_PULLUP);
+  servo1.attach(9);  // attaches the servo on pin 9 to the servo object
+  servo2.attach(11); // attaches the servo on pin 11 to the servo object
 }
 
 void loop() {
-  for (pos = 0 ; pos <= 90 ; pos += 1 ){
-    myservo.write(pos);
-    delay(15);
+  
+  bool currentState = digitalRead(ButtonPin);
+  
+  
+  if (currentState==pressed){
+    Serial.println("Button pressed");  
+    counter++;
+    delay(500);
   }
-  for (pos = 90 ; pos >= 0 ; pos -= 1 ){
-    myservo.write(pos);
-    delay(15);
+
+  
+  // switch case for handling close  and open gate when the button is pressed
+  switch(counter){
+    case 1:
+      for (pos = 0 ; pos <= 90 ; pos ++ ){
+       servo1.write(pos);
+       servo2.write(pos);
+       delay(15);
+       }
+      Serial.println("Gate opened");
+      break;
+     case 2:
+      for (pos = 90 ; pos >= 0 ; pos -= 1 ){
+      servo1.write(pos);
+      servo2.write(pos);
+      delay(15);
+      }
+      Serial.println("Gate closed");  
+      break;
+     default:
+      counter = 0;
+      servo1.write(0);
+      servo2.write(0);
+      break;
+      
+   
   }
+  
+  
+  
+  
+  
 }
+
+
+// bruh imagine this code not working 
